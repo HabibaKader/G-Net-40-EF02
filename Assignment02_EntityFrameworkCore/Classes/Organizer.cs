c1dd5f2c1d7e48f4d70d9d72b96789a181a916bc
HabibaKader <habibakader@outlook.com>
2026-04-16 02:57:29 +0200

    Organizer Class

---
+++ Assignment02_EntityFrameworkCore/Classes/Organizer.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Assignment02_EntityFrameworkCore.Classes
+{
+    public class Organizer
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string? CompanyName { get; set; }
+
+        public bool IsVerified { get; set; }
+
+        public OrganizerProfile Profile { get; set; }
+
+        public List<Event> Events { get; set; } = new();
+    }
+}
