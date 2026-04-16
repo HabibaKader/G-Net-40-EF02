e44df65fb6f6e1be3c371a9002285bca46dcf99c
HabibaKader <habibakader@outlook.com>
2026-04-16 03:11:15 +0200

    BadgeConfiguration

---
+++ Assignment02_EntityFrameworkCore/BadgeConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assignment02_EntityFrameworkCore.Classes;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Assignment02_EntityFrameworkCore
+{ 
+    public class BadgeConfiguration : IEntityTypeConfiguration<Badge>
+    {
+        public void Configure(EntityTypeBuilder<Badge> builder)
+        {
+            builder.HasKey(b => b.Id);
+
+            builder.Property(b => b.UniqueNumber)
+                   .IsRequired();
+
+            builder.HasOne(b => b.Attendee)
+                   .WithOne(a => a.Badge)
+                   .HasForeignKey<Badge>(b => b.AttendeeId);
+
+            builder.HasIndex(b => b.UniqueNumber)
+                   .IsUnique();
+        }
+    }
+}
