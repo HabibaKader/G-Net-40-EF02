906156df00b6371662d011d9c59c85dc65cc4a32
HabibaKader <habibakader@outlook.com>
2026-04-16 03:06:27 +0200

    Registration Class

---
+++ Assignment02_EntityFrameworkCore/Classes/Registration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment02_EntityFrameworkCore.Classes
+{
+    public class Registration
+    {
+        public int AttendeeId { get; set; }
+        public Attendee Attendee { get; set; }
+
+        public int EventId { get; set; }
+        public Event Event { get; set; }
+
+        public string? Note { get; set; }
+
+        public DateTime RegisteredAt { get; set; }
+    }
+}
