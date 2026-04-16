c8da0602ab8dd381e14d398310b49eeba7835fbc
HabibaKader <habibakader@outlook.com>
2026-04-16 03:02:56 +0200

    Attendee Event

---
+++ Assignment02_EntityFrameworkCore/Classes/Attendee.cs
@@ -0,0 +1,24 @@
+﻿using Azure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment02_EntityFrameworkCore.Classes
+{
+    public class Attendee
+    {
+        public int Id { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Email { get; set; }
+
+        public Address Address { get; set; }
+
+        public Badge Badge { get; set; }
+
+    }
+}
