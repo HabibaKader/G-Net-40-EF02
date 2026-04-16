f1c5edfcefafed25a1cd7e2e3008e68a1e97107c
HabibaKader <habibakader@outlook.com>
2026-04-16 02:54:27 +0200

    Add project files.

---
+++ Assignment02_EntityFrameworkCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Assignment02_EntityFrameworkCore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
