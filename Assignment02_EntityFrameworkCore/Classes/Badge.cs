fb807be03c54cc802479d7289d5c9e4a33c8eb99
HabibaKader <habibakader@outlook.com>
2026-04-16 03:04:52 +0200

    Badge Class

---
+++ Assignment02_EntityFrameworkCore/Classes/Badge.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment02_EntityFrameworkCore.Classes
+{
+    public class Badge
+    {
+        public int Id { get; set; }
+
+        public string UniqueNumber { get; set; }
+
+        public DateTime IssuedAt { get; set; }
+
+        public string Tier { get; set; } // standard / VIP
+
+        public int AttendeeId { get; set; }
+        public Attendee Attendee { get; set; }
+    }
+}
