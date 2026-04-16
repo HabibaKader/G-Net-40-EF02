6d7cfb6d27d34be4a1e87c4093aee6f95ae8f2f9
HabibaKader <habibakader@outlook.com>
2026-04-16 03:11:40 +0200

    Test

--- Assignment02_EntityFrameworkCore/Program.cs
+++ Assignment02_EntityFrameworkCore/Program.cs
@@ -6,7 +6,10 @@ namespace Assignment02_EntityFrameworkCore
     {
         static void Main(string[] args)
         {
-            
+            using var context = new AppDbContext();
+            context.Database.EnsureCreated();
+
+            Console.WriteLine("Database created successfully!");
         }
     }
 }
